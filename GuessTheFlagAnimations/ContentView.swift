35b3695d90d10f2e1ebe4629bb64b666680b70ae
Marcus Benoit <benoit.marcus@gmail.com>
2024-04-04 10:19:00 +0200

    Initial Commit

---
+++ GuessTheFlagAnimations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuessTheFlagAnimations
+//
+//  Created by Marcus Benoit on 04.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlagAnimations/GuessTheFlagAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagAnimationsApp.swift
+//  GuessTheFlagAnimations
+//
+//  Created by Marcus Benoit on 04.04.24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
